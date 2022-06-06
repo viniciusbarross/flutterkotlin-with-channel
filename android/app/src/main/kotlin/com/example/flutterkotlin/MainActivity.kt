1062befed49b0702a05344b939d46f463c77b879
vinicius barros <dev13@lwtecnologia.com.br>
2022-06-06 15:45:37 -0300

    fix: finished example how use of method channel with flutter and kotlin

--- android/app/src/main/kotlin/com/example/flutterkotlin/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterkotlin/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.flutterkotlin
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "flutter.native/helper"
@@ -11,7 +15,7 @@ class MainActivity: FlutterActivity() {
             call, result -> 
             when {
                 call.method.equals("changeColor") -> {
-                    changeColor(result)
+                    changeColor(call, result)
                 }
             }
         }
